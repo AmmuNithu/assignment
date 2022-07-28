01c27415fc8c4f5634d4483896a4e721ec6931ac
AmmuNithu <110121460+AmmuNithu@users.noreply.github.com>
2022-07-28 13:50:32 +0530

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,3 +1,9 @@
 arr=['nithra','anitha','jeeva','indhu','rammu']
 arr.reverse();
 console.log(arr);
+
+
+
+let countries=[{"name":"India","capital":"delhi","population":5000},{"name":"Pakistan","capital":"Islamabad","population":1500},{"name":"England","":"Landon","population":1000},{"name":"Canada","capital":"ottawa","population":3000}];
+let low_population_countries= countries.filter(countries=>countries.population<2000);
+console.log(low_population_countries);
